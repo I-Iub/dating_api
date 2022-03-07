e389ba79edea8a3e1b5f760c85fe8e4fe5f488b0
rashid <ri-post@yandex.ru>
2022-03-07 16:25:20 +0500

    3. Добавил наложение водяного знака на аватарку при регистрации.

--- dating/api/fields.py
+++ dating/api/fields.py
@@ -3,6 +3,8 @@ from base64 import b64decode
 from django.core.files.base import ContentFile
 from rest_framework import serializers
 
+from api.utils import watermark
+
 
 class Base64ToImageField(serializers.ImageField):
     def to_internal_value(self, base64_string):
@@ -11,10 +13,9 @@ class Base64ToImageField(serializers.ImageField):
         if base64_string.startswith('data:image'):
             description, image_string = base64_string.split(';base64,')
             extension = description.split('/')[-1]  # расширение файла
-            data = ContentFile(
-                b64decode(image_string), name='temp.' + extension
-            )
         else:
             image_string = base64_string
-            data = ContentFile(b64decode(image_string), name='temp.jpeg')
+            extension = 'png'
+        bytes_image = watermark(b64decode(image_string))
+        data = ContentFile(bytes_image, name='temp.' + extension)
         return super().to_internal_value(data)
---
+++ dating/api/utils.py
@@ -0,0 +1,31 @@
+import io
+import os
+
+from django.conf import settings
+from PIL import Image
+from PIL import ImageEnhance
+
+ENCHANCE_FACTOR = 0.1
+ROTATE = -30
+
+
+def watermark(source):
+    """Получает картинку в виде bytes-like-object, накладывает водяной знак и
+    возвращает её в виде bytes-like-object.
+    """
+    image = Image.open(io.BytesIO(source))
+    mark = Image.open(
+        os.path.join(settings.BASE_DIR, 'media/watermark/heart-love-red.jpg')
+    )
+    mark = mark.resize(image.size)
+    alpha = ImageEnhance.Brightness(mark.split()[2]).enhance(ENCHANCE_FACTOR)
+    mark.putalpha(alpha)
+    mark = mark.rotate(ROTATE, Image.BICUBIC)
+    if image.mode != 'RGBA':
+        image = image.convert('RGBA')
+    if mark.mode != 'RGBA':
+        mark = mark.convert('RGBA')
+    watermark_image = Image.alpha_composite(image, mark)
+    buffer = io.BytesIO()
+    watermark_image.save(buffer, format='PNG')
+    return buffer.getvalue()
